f622e6542ab53fd9bf3bd9b5fe41be3425e7ffc9
Kozulkevych Nadiia <nkozulkevych@gmail.com>
2023-01-31 15:05:57 +0200

    add

--- src/js/fetchCards.js
+++ src/js/fetchCards.js
@@ -9,7 +9,7 @@ let total = 0;
 export async function fetchCards(query) {
   const response = await axios({
     method: 'get',
-    url: `https://pixabay.com/api/?key=33168858-09b17812fdd05775e42993a92&q=${query}&page=1&per_page=${limit}&image_type=photo&orientation=horizontal&safesearch=true`,
+    url: `https://pixabay.com/api/?key=33173109-7f033f41c702edaff07185b5d&q=${query}&page=1&per_page=${limit}&image_type=photo&orientation=horizontal&safesearch=true`,
   });
   total = response.data.totalHits;
   totalPages = Math.ceil(total / limit);
@@ -20,7 +20,7 @@ export async function fetchCards(query) {
 export async function moreFetchCards(query) {
   const response = await axios({
     method: 'get',
-    url: `https://pixabay.com/api/?key=33168858-09b17812fdd05775e42993a92&q=${query}&page=${page}&per_page=${limit}&image_type=photo&orientation=horizontal&safesearch=true`,
+    url: `https://pixabay.com/api/?key=33173109-7f033f41c702edaff07185b5d&q=${query}&page=${page}&per_page=${limit}&image_type=photo&orientation=horizontal&safesearch=true`,
   });
   page += 1;
   return response.data;
